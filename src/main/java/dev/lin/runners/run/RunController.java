014abf5328a901c48a9e3567afa8bc3edd103c0c
Tony <TonyLin3591@gmail.com>
2024-04-26 00:42:15 -0400

    crud

--- src/main/java/dev/lin/runners/run/RunController.java
+++ src/main/java/dev/lin/runners/run/RunController.java
@@ -1,13 +1,16 @@
 package dev.lin.runners.run;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@RequestMapping("/api/runs")
+
 public class RunController {
 
     //private List<Run> runs = new ArrayList<>();
@@ -21,5 +24,38 @@ public class RunController {
     @GetMapping("/api/runs")
     List<Run> findAll(){
         return runRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Run findById(@PathVariable Integer id){
+
+        Optional<Run> run = runRepository.findById(id);
+        if(run.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return run.get();
+    }
+
+
+    //post
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@RequestBody Run run){
+
+        runRepository.create(run);
+    }
+
+    //put
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@RequestBody Run run,Integer id){
+        runRepository.update(run,id);
+
+    }
+    //delete
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping
+    void delete(@PathVariable Integer id){
+        runRepository.delete(id);
     }
 }
--- src/main/java/dev/lin/runners/run/RunRepository.java
+++ src/main/java/dev/lin/runners/run/RunRepository.java
@@ -7,12 +7,18 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.time.temporal.ChronoUnit;
+import java.util.Optional;
 
 @Repository
 public class RunRepository {
     private List<Run> runs = new ArrayList<>();
+
     List<Run> findAll(){
         return runs;
+    }
+
+    void create(Run run){
+        runs.add(run);
     }
 
     @PostConstruct
@@ -31,5 +37,22 @@ public class RunRepository {
                 LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
                 8,
                 Location.INDOOR));
+    }
+
+    Optional<Run> findById(Integer id){
+        return runs.stream()
+                .filter(run -> run.id() == id)
+                .findFirst();
+    }
+
+    void update(Run run,Integer id){
+        Optional<Run> existingRun = findById(id);
+        if(existingRun.isPresent()){
+            runs.set(indexOf(existingRun.get()),run)
+        }
+    }
+
+    void delete(Integer id){
+        runs.removeIf(run -> run.id().equals(id));
     }
 }
